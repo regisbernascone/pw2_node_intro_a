0a3125c6421951b57ef0156bd67cdb74f15413f5
regisbernascone <regis.bernascone@etec.sp.gov.br>
2024-06-17 20:45:06 -0300

    Aula1706 Sessão e Segurança

---
+++ Aula1706/pw2_node_intro_a-55-deploy-de-estrutura-azure-nodejs-e-endpoint/ideiaapp/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const flash = require('express-flash')
+const conn = require('./db/conn')
+
+const app = express()
+
+const Ideia = require('./models/Ideia')
+const authRouter = require('./routes/authRoutes')
+const ideiasRoutes = require('./routes/ideiaRoutes')
+const IdeiaController = require('./controllers/IdeiaController')
+
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+app.use(express.json())
+app.use(
+  session({
+    name:'session',
+    secret:'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('patch').join(requiere('os').tmdir(), 'session'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now()+ 3600000),
+      httpOnly: true,
+    }
+  }),
+)
+app.use(flash())
+app.use(express.static('public'))
+app.use((req, res, next) => {
+  console.log(req.session.userid)
+if(req.session.userid){
+  res.locals.session = req.session
+}
+next()
+})
+
+app.use('/ideias', ideiasRoutes)
+app.use('/', authRouter)
+
+
+app.get('/', IdeiaController.showIdeias)
+
+conn
+.sync()
+.then(() =>{
+    app.listen(3000, () => {
+        console.log('Servidor operando na porta local: http://127.0.0.1:3000')
+    })
+})
